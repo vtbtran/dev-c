c96deb0bc58822ceaf590b8f9f60c0cdecc77ab5
baotran <vothibaotran01012005@gmail.com>
2023-09-22 22:23:04 +0700

    init project c++

---
+++ hello_world/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  hello_world
+//
+//  Created by Võ Thị Bảo Trân on 22/09/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
